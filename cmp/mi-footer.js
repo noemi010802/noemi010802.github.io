dac165dae3ff66e231b39f91392ad599bf9e3689
noemi010802 <82332421+noemi010802@users.noreply.github.com>
2021-04-11 16:07:54 -0500

    Delete mi-footer.js

--- cmp/mi-footer.js
+++
@@ -1,7 +0,0 @@
-class MiFooter extends HTMLElement{
-  connectedCallback(){
-    this.innerHTML=/*hmtl*/
-      "Copyright &copy; 2021 Noemi Morales Andrade.";
-  }
-}
-custoElements.define("mi-footer", Mifooter);
